84ad5fa7edf7a73f1e55fed3c2703f649a20a317
camian0 <camian6@outlook.com>
2025-08-22 19:56:53 -0500

    feat: changed use effect for onPatched

--- customized_addons/global_discount/static/src/js/order_widget.js
+++ customized_addons/global_discount/static/src/js/order_widget.js
@@ -2,22 +2,23 @@
 // import { OrderSummary } from "@point_of_sale/app/screens/product_screen/order_summary/order_summary";
 import { OrderWidget } from "@point_of_sale/app/generic_components/order_widget/order_widget";
 import { patch } from "@web/core/utils/patch";
-import { useEffect } from "@odoo/owl";
+import { onPatched } from "@odoo/owl";
 
 patch(OrderWidget.prototype, {
   setup() {
     super.setup();
-    // this.oldLinesLength = 0
-    // this.currentLinesLength = this.currentOrder.lines.length;
+    this.oldLinesLength = 0
+    this.currentLinesLength = this.props.lines.length;
 
-    useEffect(() => {
-      console.log("useEffect ejecutándose");
-      if (this.props && this.props.lines.length > 0) {
+    // check lisr products
+    onPatched(() => {
+      this.currentLinesLength = this.props.lines.length || 0;
+
+      if (this.oldLinesLength !== this.currentLinesLength) {
+        console.log("Productos cambiaron:", this.oldLinesLength, "→", this.currentLinesLength);
+        this.oldLinesLength = this.currentLinesLength;
         this.recalculateDiscount();
       }
-    }, () => {
-      // 3. ESTAS son las dependencias que OWL monitorea
-      return this.props ? [this.props.lines.length > 0] : [];
     });
 
 
@@ -28,21 +29,27 @@ patch(OrderWidget.prototype, {
     // let newOrderLines = []
     if (this.props && this.props.lines.length > 0) {
       console.log("hay productos en la orden")
-      let discount = this.searchProductDiscount(this.order.lines)
-      if (discount) {
-        // let newTotal = this.order.amount_total - discount.price_unit;
-        // // newTotal = this.roundMoneyWithThreshold(newTotal);
-        // // console.log("nuevo total con redondeo: ", newTotal)
-        // console.log("nuevo total: ", newTotal)
-        // console.log("descuento: ", discount)
-        // this.order.amount_total = this.order.amount_total - newTotal;
-        // newOrderLines = this.order.lines.map((item) => {
-        //   let discount=0
-        //   let discountProduct = item.
+      // let discount = this.searchProductDiscount(this.props.lines)
+      // if (discount) {
+      //   let oldTotal = this.props.taxTotals.total_amount
+      //   // this.props.taxTotals = this.props.taxTotals * 1
+      //   // let newTotal = oldTotal - discount.price_unit;
+      //   console.log("total antes: ", oldTotal)
+      //   console.log("total despues: ", newTotal)
+      //   this.props.taxTotals.order_total = oldTotal
 
-        //   return item;
-        // })
-      }
+      //   // newTotal = this.roundMoneyWithThreshold(newTotal);
+      //   // console.log("nuevo total con redondeo: ", newTotal)
+      //   // console.log("nuevo total: ", newTotal)
+      //   // console.log("descuento: ", discount)
+      //   // this.order.amount_total = this.order.amount_total - newTotal;
+      //   // newOrderLines = this.order.lines.map((item) => {
+      //   //   let discount=0
+      //   //   let discountProduct = item.
+
+      //   //   return item;
+      //   // })
+      // }
     }
 
   },
@@ -50,11 +57,12 @@ patch(OrderWidget.prototype, {
   searchProductDiscount(products) {
     if (products) {
       for (let product of products) {
-        let productName = product.product_id.display_name.toUpperCase().trim();
+        let productName = product.full_product_name.toUpperCase().trim();
         console.log("nombre producto: ", productName)
         if (productName === "DISCOUNT") {
           product.price_unit = product.price_unit * -1
           product.price_subtotal = product.price_subtotal * -1
+          product.price_subtotal_incl = product.price_subtotal_incl * -1
           console.log("producto cambiado: ", product)
           return product;
         }
